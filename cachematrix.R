3ecad4e058c7b238169acfef33a7d6cd904670eb
Claire Garza <badG_22@yahoo.com>
2015-11-17 11:02:37 -0700

    Variable Names and Message
    Fixed the variable names that were getting sent as input.. that’s just
    my bad for being absolutely dumb.. and added “getting cached data”
    message for confirmation the if statement was entered

--- cachematrix.R
+++ cachematrix.R
@@ -27,10 +27,11 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if (!is.null(inv)){
+    message("getting cached data")
     return(inv)
   }
   data <- x$get()
-  inv <- solve(x)
-  x$setinverse(m)
+  inv <- solve(data)
+  x$setinverse(inv)
   inv
 }
